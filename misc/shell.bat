d2d780906ab1bbc0bb3c156176b3bee1b5b598a5
DanielChambi <daniel.chambi.campos@alumnos.upm.es>
2022-04-04 11:53:05 +0200

    First Commit. Draw to window with Win32. Basic Rasterizer rendering and sample figure.

---
+++ misc/shell.bat
@@ -0,0 +1,4 @@
+@echo off
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+set path=C:\Program Files\Microsoft VS Code\bin;%path%
+set path=W:\repos\TFG\misc;%path%
